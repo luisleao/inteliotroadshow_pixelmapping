507e9123975cfa519235c5d3b1990c336986a04a
Luis Leao <luis.leao@gmail.com>
2015-06-19 18:28:08 -0300

    leds

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+var UNIVERSE = 0;
+
+var LED_PIN = 3;
+
+
 var artnetsrv = require('artnet-node/lib/artnet_server');
+
+
+var mraa = require('mraa');
+var pin = new mraa.Pwm(LED_PIN);
+pin.enable(false);
+pin.write(0);
+
+
 
 var srv = artnetsrv.listen(6454, function(msg, peer) {
 	console.log("-----------------");
@@ -8,6 +21,18 @@ var srv = artnetsrv.listen(6454, function(msg, peer) {
 	console.log("Length: " + msg.length);
 	console.log("Data: " + msg.data);
 	console.log("-----------------");
+
+
+	if (msg.universe == UNIVERSE && msg.length > 0) {
+		if (msg.data[0] == 0) {
+			pin.enable(false);
+		} else {
+			pin.enable(true);
+			pin.write(msg.data[0]/255);
+		}
+		
+	}
+
 });
 
 
